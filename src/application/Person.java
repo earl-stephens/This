a806da1b61172d1d9017f2b7f19e32d5a618839f
earl-stephens <stephensearl@hotmail.com>
2021-12-26 14:50:30 -0700

    Initial commit

---
+++ src/application/Person.java
@@ -0,0 +1,23 @@
+package application;
+
+public class Person {
+	String name;
+	int age;
+	
+	//setter
+	void setName(String theName) {
+		name = theName;
+	}
+	
+	void setAge(int theAge) {
+		age = theAge;
+	}
+	
+	String getName() {
+		return name;
+	}
+	
+	int getAge() {
+		return age;
+	}
+}
